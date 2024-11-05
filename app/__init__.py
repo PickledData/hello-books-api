899c892fe3bcd10a92df309ad43c6a5047f70d96
somy <smycho28@gmail.com>
2024-11-04 20:48:58 -0800

    refac toring of validate function and dictionary of query headers

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,20 @@
 from flask import Flask
 from .db import db, migrate
 from .models import book
-from .routes.book_routes import books_bp
+import os
+from .routes.book_routes import bp as books_bp
 
-def create_app():
+
+def create_app(config=None):
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+        "postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development"
+    )
+
+    if config:
+        app.config.update(config)
 
     db.init_app(app)
     migrate.init_app(app, db)
